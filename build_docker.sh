54d7fbb8b0f3275732f5d43a55c477004f4b484d
m <manapragada@telecom-paris.fr>
2024-02-17 17:03:12 +0100

    Moved to docker-compose

--- build_docker.sh
+++ build_docker.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 
-# Default values for optional arguments
-CONTAINER_NAME="dev0"
-SSH_PATH="$HOME/.ssh"
-GITCONFIG_PATH="$HOME/.gitconfig"
-
-# Function to display usage
-usage() {
-    echo "Usage: $0 [container name] [ssh path] [gitconfig path]"
-    echo " - Container name: Name of the Docker container (default: dev0)"
-    echo " - SSH path: Path to the SSH directory (default: $HOME/.ssh)"
-    echo " - Gitconfig path: Path to the .gitconfig file (default: $HOME/.gitconfig)"
-}
-
-# Assign arguments to variables, if provided
-if [ "$#" -ge 1 ]; then
-    CONTAINER_NAME=$1
-fi
-if [ "$#" -ge 2 ]; then
-    SSH_PATH=$2
-fi
-if [ "$#" -ge 3 ]; then
-    GITCONFIG_PATH=$3
-fi
-
-# Build the Docker image
-docker build -t linux-dev-env .
+docker-compose up -d --build
 
--- run_docker.sh
+++ run_docker.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-# Default container name
-CONTAINER_NAME="dev0"
-
-# Check for an optional container name argument
-if [ "$#" -eq 1 ]; then
-    CONTAINER_NAME=$1
-fi
-
-# Check if the container exists
-container_exists=$(docker ps -a -q -f name=^/${CONTAINER_NAME}$)
-if [ ! -z "$container_exists" ]; then
-    echo "Container $CONTAINER_NAME exists."
-    # Check if the container is running
-    if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "false" ]; then
-        echo "Container $CONTAINER_NAME is stopped. Starting it..."
-        docker start $CONTAINER_NAME
-    else
-        echo "Container $CONTAINER_NAME is already running."
-    fi
-else
-    echo "Container $CONTAINER_NAME does not exist. Creating and starting it..."
-    # Run the Docker container in detached mode
-    docker run -d -v "$HOME/.ssh:/root/.ssh" -v "$HOME/.gitconfig:/root/.gitconfig" --name "$CONTAINER_NAME" linux-dev-env
-fi
-
-# Attach to the container interactively
-docker exec -it "$CONTAINER_NAME" /bin/bash
+docker-compose exec dev0 /bin/bash
 
