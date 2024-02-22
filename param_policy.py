6a09d3769e90cf752b94a2015db836a85e91e5de
m <manapragada@telecom-paris.fr>
2024-02-22 10:28:44 +0000

    Added RandomTree stream. Refactored the configuration.

--- param_policy.py
+++ param_policy.py
@@ -9,23 +9,31 @@ import numpy as np
 # CONSTANTS
 CONFIG = {
     'random_seed': 0,
-    'change_point': 10000,
-    'evaluation_interval': 1000,
-    'max_examples': 20000,
-    'delta_easy': 1e-1,
-    'delta_hard': 1e-7,
-    'seed0': 0,
-    'seed1': 100,
-    'update_delta_accuracy_threshold': 0.9,
-    'num_runs': 2,
-    'stream_type': 'RandomRBF',
+    'num_runs': 10,
     'model': 'UpdatableEFDTClassifier',
-    'preinitialized_params_random_rbf': {
-        'n_classes': 3,
-        'n_features': 3,
-        'n_centroids': 3,
-    },
     'update_delta_when_accuracy_drops': True,
+    'update_delta_accuracy_threshold': 0.9,
+
+    'stream': {
+        'type': 'RandomRBF',
+        'change_point': 10000,
+        'max_examples': 20000,
+        'evaluation_interval': 1000,
+        'seeds': {
+            'initial': 0,
+            'drift': 100,
+        },
+        'params': {
+            'n_classes': 5,
+            'n_features': 3,
+            'n_centroids': 5,
+        },
+    },
+
+    'model_params': {
+        'delta_easy': 4e-1,
+        'delta_hard': 1e-7,
+    },
 }
 
 # Set random seeds
@@ -40,7 +48,7 @@ def prequential_evaluation(model, stream, config):
     correct_predictions = 0
     total_predictions = 0
     prev_accuracy = 0
-    for x, y in stream.take(config['max_examples']):
+    for x, y in stream.take(config['stream']['max_examples']):
         prediction = model.predict_one(x)
         model.learn_one(x, y)
 
@@ -49,7 +57,7 @@ def prequential_evaluation(model, stream, config):
         total_predictions += 1
 
         step += 1
-        if step % config['evaluation_interval'] == 0:
+        if step % config['stream']['evaluation_interval'] == 0:
             accuracy = correct_predictions / total_predictions
             accuracy_change = accuracy - prev_accuracy
             prev_accuracy = accuracy
@@ -61,12 +69,13 @@ def prequential_evaluation(model, stream, config):
             total_predictions = 0
 
             if config['update_delta_when_accuracy_drops']:
-                if accuracy < config['update_delta_accuracy_threshold']:
-                    model.update_delta(config['delta_easy'])
-                else:
-                    model.update_delta(config['delta_hard'])
+                if 'update_delta_accuracy_threshold' in config['model_params']:
+                    if accuracy < config['model_params']['update_delta_accuracy_threshold']:
+                        model.update_delta(config['model_params']['delta_easy'])
+                    else:
+                        model.update_delta(config['model_params']['delta_hard'])
 
-    evaluation_steps = list(range(config['evaluation_interval'], config['max_examples'] + 1, config['evaluation_interval']))
+    evaluation_steps = list(range(config['stream']['evaluation_interval'], config['stream']['max_examples'] + 1, config['stream']['evaluation_interval']))
     data = list(zip(evaluation_steps, accuracy_changes[0:], accuracies[0:]))
     df = pd.DataFrame(data, columns=['Evaluation Step', 'Change in Accuracy', 'Accuracy'])
 
@@ -76,6 +85,8 @@ def data_stream_template_factory(stream_type, preinitialized_params):
     def constructor(seed):
         if stream_type == 'RandomRBF':
             return RandomRBF(seed_model=seed, seed_sample=seed, **preinitialized_params)
+        elif stream_type == 'RandomTree':
+            return synth.RandomTreeGenerator(seed=seed, **preinitialized_params)
         else:
             raise ValueError(f"Unknown stream type: {stream_type}")
     return constructor
@@ -84,24 +95,23 @@ import concurrent.futures
 import concurrent.futures
 
 def run_experiment(config, seed0, seed1):
-    stream_factory = data_stream_template_factory(config['stream_type'], config['preinitialized_params_random_rbf'])
+    stream_factory = data_stream_template_factory(config['stream']['type'], config['stream']['params'])
 
     concept_drift_stream = ConceptDriftStream(
         stream=stream_factory(seed=seed0), 
         drift_stream=stream_factory(seed=seed1), 
-        position=config['change_point'], 
+        position=config['stream']['change_point'], 
         seed=seed0
     )
 
     ModelClass = globals()[config['model']]
-    model = ModelClass(delta=config['delta_hard'])
+    model = ModelClass(delta=config['model_params']['delta_hard'])
 
     return prequential_evaluation(model, concept_drift_stream, config)
 
 def run_seeded_experiments(config):
     dfs = []
-    seed0, seed1 = config['seed0'], config['seed1']
-
+    seed0, seed1 = config['stream']['seeds']['initial'], config['stream']['seeds']['drift']
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = [executor.submit(run_experiment, config, seed0 + i, seed1 + i) for i in range(config['num_runs'])]
         for future in futures:
@@ -127,8 +137,8 @@ def run_experiments_with_different_policies(config):
         df_false = results_false[i]
         df_true = results_true[i]
 
-        avg_accuracy_false = df_false[df_false['Evaluation Step'] > config['change_point']]['Accuracy'].mean()
-        avg_accuracy_true = df_true[df_true['Evaluation Step'] > config['change_point']]['Accuracy'].mean()
+        avg_accuracy_false = df_false[df_false['Evaluation Step'] > config['stream']['change_point']]['Accuracy'].mean()
+        avg_accuracy_true = df_true[df_true['Evaluation Step'] > config['stream']['change_point']]['Accuracy'].mean()
 
         print(f"Experiment {i+1}:")
         print(f"Average accuracy after drift with 'update_delta_when_accuracy_drops' set to False: {avg_accuracy_false}")
