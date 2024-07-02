79cb974c20ed98178c2239a605369e1720fc4571
Chaitanya Manapragada <cmanapragada@lamedell16.enst.fr>
2024-07-02 20:56:40 +0200

    changed agents.py to use a random generator with a seed, and printing seed picked shows that when all Q-values in the row for a state are 0, then the 0th action is picked. will try changing this.

--- agents.py
+++ agents.py
@@ -14,10 +14,13 @@ class BaseAgent:
             # If state is None, or with probability epsilon, return a random action
             # return np.random.choice(self.num_actions) # Try this instead later
             # return np.random.randint(self.num_actions)
-            return self.rng.integers(self.num_actions)
+            action_no = self.rng.integers(self.num_actions)
+            print("Initial or exploratory action " + str(action_no))
         else:
             # Otherwise, select action greedily based on current Q-values for the given state
-            return np.argmax(self.Q_table[state])
+            action_no = np.argmax(self.Q_table[state])
+            print("Best Q-table action" + str(action_no))
+        return action_no
 
 class QLearningAgent(BaseAgent):
     def __init__(self, num_states, num_actions, alpha=0.1, gamma=0.9, epsilon=0.1):
