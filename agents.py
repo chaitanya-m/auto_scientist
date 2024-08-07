c07399aa4c8f362625e8ce0d184817332b7f9038
m <manapragada@telecom-paris.fr>
2024-08-07 10:43:31 +0000

    select_action takes a state, not an accuracy change bin

--- agents.py
+++ agents.py
@@ -8,22 +8,20 @@ class BaseAgent:
         self.Q_table = np.zeros((num_accuracy_change_bins, num_actions))  # Initialize Q-values to zeros
         self.rng = np.random.default_rng(0)
 
-    def select_action(self, accuracy_change_bin):
+    def select_action(self, state):
         # Select action using epsilon-greedy policy
-        if accuracy_change_bin is None or np.random.rand() < self.epsilon:
-            # If accuracy change bin is None, or with probability epsilon, return a random action
-            # return np.random.choice(self.num_actions) # Try this instead later
-            # return np.random.randint(self.num_actions)
+        if np.random.rand() < self.epsilon:
+            # With probability epsilon, return a random action
             action_no = self.rng.integers(self.num_actions)
             # print("Initial or exploratory action " + str(action_no))
 
-        elif np.amax(self.Q_table[accuracy_change_bin]) == np.amin(self.Q_table[accuracy_change_bin]):
+        elif np.amax(self.Q_table[state]) == np.amin(self.Q_table[state]): # If all Q-values are equal
             action_no = self.rng.integers(self.num_actions)
             # print("Actions all still equal " + str(action_no))
 
         else:
             # Otherwise, select action greedily based on current Q-values for the given accuracy change bin
-            action_no = np.argmax(self.Q_table[accuracy_change_bin])
+            action_no = np.argmax(self.Q_table[state])
             # print("Best Q-table action " + str(action_no))
         return action_no
 
